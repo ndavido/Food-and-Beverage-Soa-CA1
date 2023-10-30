71ee8c3b0cfbbc19787f59defa32c4f9b8d12ce2
ndavido <D00239107@student.dkit.ie>
2023-10-30 19:19:30 +0000

    :sparkles: Got Json Data to display

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Beverages = new System.Windows.Forms.TextBox();
+            this.Food = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Beverages
+            // 
+            this.Beverages.Location = new System.Drawing.Point(107, 56);
+            this.Beverages.Multiline = true;
+            this.Beverages.Name = "Beverages";
+            this.Beverages.Size = new System.Drawing.Size(379, 402);
+            this.Beverages.TabIndex = 0;
+            this.Beverages.TextChanged += new System.EventHandler(this.Beverages_TextChanged);
+            // 
+            // Food
+            // 
+            this.Food.Location = new System.Drawing.Point(626, 56);
+            this.Food.Multiline = true;
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(379, 402);
+            this.Food.TabIndex = 1;
+            this.Food.TextChanged += new System.EventHandler(this.Food_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1188, 503);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Beverages);
             this.Name = "Form1";
             this.Text = "Food & Bevarage Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox Beverages;
+        private TextBox Food;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using RestSharp.Serializers;
+using RestSharp;
+
 namespace Food_and_Beverage
 {
     public partial class Form1 : Form
@@ -5,6 +9,43 @@ namespace Food_and_Beverage
         public Form1()
         {
             InitializeComponent();
+            Load_Beverages();
+            Load_Food();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Load_Beverages()
+        {
+            var client = new RestClient("https://www.thecocktaildb.com/api/json/v1/1/search.php?s");
+            var request = new RestRequest();
+
+            var response = client.Execute(request);
+
+            Beverages.Text = response.Content;
+        }
+
+        private void Load_Food()
+        {
+            var client = new RestClient("https://www.themealdb.com/api/json/v1/1/search.php?s");
+            var request = new RestRequest();
+
+            var response = client.Execute(request);
+
+            Food.Text = response.Content;
+        }
+
+        private void Beverages_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Food_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
