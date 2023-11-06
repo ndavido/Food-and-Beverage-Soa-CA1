460bea2d4d12198117c8b1b62e4a27364144b69d
ndavido <D00239107@student.dkit.ie>
2023-11-06 10:05:16 +0000

    :sparkles: Changed design in Home page

--- UserControls/HomeControl.Designer.cs
+++ UserControls/HomeControl.Designer.cs
@@ -29,22 +29,73 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(348, 165);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 54);
+            this.label1.Size = new System.Drawing.Size(788, 54);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Home Page";
+            this.label1.Text = "Welcome To Meal and Beverage Selector";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(508, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "required ingrediants, and instructions on how to prepare.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(3, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(807, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Click on the Meal or Beverage in the list and it will display you an image of how" +
+    " it looks like,\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(3, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(858, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "You can search through our collection of meals and beverages by navigating throug" +
+    "h the nav bar.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(3, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(787, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "If you like any of the recipes, you can save it and later view it in the Saved Re" +
+    "cipes section.";
             // 
             // HomeControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "HomeControl";
             this.Size = new System.Drawing.Size(936, 384);
@@ -56,5 +107,9 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
